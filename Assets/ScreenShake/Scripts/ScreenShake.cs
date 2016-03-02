00610844652ca4fba1b85b36b3c7e9f37a433dd4
Nicholas Scheurich <nick@scheurich.me>
2016-03-01 18:18:53 -0600

    First pass at screen shake

---
+++ Assets/ScreenShake/Scripts/ScreenShake.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Rustic
+{
+    public class ScreenShake : MonoBehaviour
+    {
+	public float _xAmount;
+	public float _yAmount;
+	public float _duration;
+	public float _frequency;
+
+	private Camera camera;
+	private Vector3 cameraInitPos;
+
+	void Start()
+	{
+	    camera = Camera.main;
+	    cameraInitPos = camera.transform.position;
+	    StartCoroutine(Shake());
+	}
+
+	IEnumerator Shake()
+	{
+	    var time = 0f;
+
+	    while(time < _duration) {
+		time += Time.deltaTime + _frequency;
+
+		var xPos = Random.Range(1, _xAmount);
+		var yPos = Random.Range(1, _yAmount);
+		var zPos = camera.transform.position.z;
+
+		camera.transform.position = new Vector3(xPos, yPos, zPos);
+
+		yield return new WaitForSeconds(_frequency);
+	    }
+
+	    camera.transform.position = cameraInitPos;
+
+	    Destroy(gameObject);
+	}
+    }
+}
---
+++ Assets/ScreenShake/Scripts/Setup.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Setup : MonoBehaviour
+{
+    public UnityEngine.UI.Button _button;
+    public Rustic.ScreenShake _screenShake;
+
+    void Start () {
+	_button.onClick.AddListener(() => { Instantiate(_screenShake); });
+    }
+}
