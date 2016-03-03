ca3278414afb22c2e5e5d6c989d6726296fe5367
jkwascom <jkwascom@gmail.com>
2016-03-03 12:24:08 -0600

    Basic UI Examples for Title and HUD

---
+++ Assets/UIOverlay/QuickHUD.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class QuickHUD : MonoBehaviour {
+    public HorizontalLayoutGroup outputTarget;
+    public Text outputPrefab;
+    void Reset ()
+    {
+        outputTarget = this.gameObject.GetComponent<HorizontalLayoutGroup>();
+    }
+
+    public Text registerHudElement()
+    {
+        var t = Object.Instantiate(outputPrefab);
+        t.transform.SetParent(outputTarget.transform, false);
+        return t;
+    }
+}
